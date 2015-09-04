ca57bb973a8b0af1d485ed4d968a22f0f13435e7
plsxysam <418228079@qq.com>
2015-09-04 18:46:03 +0800

    URL路由配置实现超短URL地址

--- APP/Conf/config.php
+++ APP/Conf/config.php
@@ -14,6 +14,16 @@ return array(
 	'APP_GROUP_PATH' => 'Modules',
 
 	//加载独立配置项文件
-	'LOAD_EXT_CONFIG' => 'verify'
+	'LOAD_EXT_CONFIG' => 'verify',
+
+	//URL路由 'URL_MODEL' => 2, 0普通 1正常 2URL路由 
+	//apache 配置mod_rewrite.so和AllowOverride
+	'URL_MODEL' => 2, 
+	'URL_ROUTER_ON' => true,
+	'URL_ROUTE_RULES' => array(
+		//正则中\d代表数字	正则路由/^c_(\d+)$/   Index/List/index?id=:1
+		'/^c_(\d+)$/' => 'Index/List/index?id=:1',
+		':id\d' => 'Index/Show/index'
+		)
 );
 ?>
